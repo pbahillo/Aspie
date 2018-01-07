a0798d3765e14b576ce6348345203e027fcdb3d4
pbahillo <pbahillo@outlook.com>
2018-01-07 17:57:08 +0100

    Creadas las diferentes actividades de la app Añadidas imágenes de logo

---
+++ app/src/main/java/eus/ehu/tta/pbahillo002/aspie/Game2Activity.java
@@ -0,0 +1,13 @@
+package eus.ehu.tta.pbahillo002.aspie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game2);
+    }
+}
